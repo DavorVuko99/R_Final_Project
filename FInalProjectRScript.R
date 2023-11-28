5be0113a3a6abd488d87d585c49f7f422aace2f3
ddahal1 <diwas.dahal@uvm.edu>
2023-11-27 20:13:46 -0500

    Cleared the data

--- FInalProjectRScript.R
+++ FInalProjectRScript.R
@@ -24,6 +24,67 @@ BurlingtonCrimeData <- read.csv(ArrestsDataScienceFinal.csv)
 ```
 
 
+## Clearing the data
+
+```{r reading the data}
+library(lubridate)
+
+ArrestsData <- 
+  BurlingthonArrestsData
+  colnames(ArrestsData) <- 
+    tolower(colnames(ArrestsData))
+  
+  ArrestsData <- ArrestsData |>
+  mutate(  # Convert date to date type
+    year = year(arrest_date),
+    month = month(arrest_date),
+    month_label = month.abb[month(arrest_date)],
+    day = yday(arrest_date)
+  ) 
+  
+  #clearing the gender data
+  ArrestsData <- ArrestsData |>
+    mutate(gender = case_when(
+    gender == "Male - M" ~ "M",
+    gender == "Female - F" ~ "F",
+    TRUE ~ gender  # Keep other values unchanged
+  ))
+  
+  #clearing the race data
+  ArrestsData <- ArrestsData |>
+    mutate(race = case_when(
+    race == "White - W" ~ "White",
+    race == "Black or African American - B" ~ "Black",
+    race == "Asian - A" ~ "Asian",
+    TRUE ~ race  # Keep other values unchanged
+  ))
+  
+  
+  #clearing the ethnicity data
+  ArrestsData <- ArrestsData |>
+    mutate(ethnicity = case_when(
+    ethnicity == "Not Hispanic or Latino - N" ~ "N",
+    ethnicity == "Hispanic or Latino - H" ~ "Y",
+    ethnicity == "Unknown - U" ~ "NA",
+    
+    TRUE ~ ethnicity  # Keep other values unchanged
+  ))
+  
+  #clearing the charges data so we know what type of charge it was
+  #ArrestsData <- ArrestsData |>
+    #mutate(spc_charge = str_extract(charge, "[0-9-].*$"))
+  
+  # Filter data for the year 2018 and later
+  ArrestsData <- 
+    ArrestsData |>
+    filter(year >= 2018)
+  
+  
+tibble(ArrestsData)
+
+```
+
+
 
 
 
